c860302735c36ad61c058b1998d04be795424fa2
Johnnie Munger <johnnietheblack@gmail.com>
2013-10-09 09:14:42 -0700

    init

---
+++ jquery.plugin.js
@@ -0,0 +1,58 @@
+/**
+ * PluginName - jQuery Plugin
+ * version 1.0.0
+ * 
+ * Copyright 2013 Wonderful Co.
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ **/
+
+;(function($) {
+	
+	var PluginName = function(el,options) {
+		
+		var $pluginName = this;
+		
+		var settings = $.extend({}, $.fn.pluginName.defaults, options);
+		var vars = {
+			/* internal plugin vars */
+		};
+		
+		// define main elements
+		var $parent = $(el);
+		
+		var setup = function() {
+			setupParent();
+			setupChildren();
+			setupControls();
+			
+			// callback
+			settings.afterLoad.call(this,vars);
+		}
+		
+        var trace = function(msg){
+			if(settings.debug) {
+            	if(this.console && typeof console.log !== 'undefined') { console.log(msg); }
+			}
+        }
+
+		setup();
+	}
+	
+	// plug it in
+	$.fn.pluginName = function(options) {
+        return this.each(function(key, value){
+            var element = $(this);
+            // Return early if this element already has a plugin instance
+            if (element.data('pluginName')) { return element.data('pluginName'); }
+            // Pass options to plugin constructor
+            var pluginName = new PluginName(this, options);
+            // Store plugin object in this element's data
+            element.data('pluginName', pluginName);
+        });
+    };
+
+	$.fn.pluginName.defaults = {
+		/* plugin settings */
+    };
+	
+}(jQuery));
